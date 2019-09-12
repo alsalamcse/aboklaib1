b0c5e4f15320c5d00a28e5a8f6bafe345551cf3f
Owner <ekrmaawad231@gmail.com>
2019-09-12 12:05:26 +0300

    Initial commit //lkkkk

---
+++ app/src/main/java/com/abo/aboklaib1/aboklaib1.java
@@ -0,0 +1,13 @@
+package com.abo.aboklaib1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class aboklaib1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_aboklaib1 );
+    }
+}
